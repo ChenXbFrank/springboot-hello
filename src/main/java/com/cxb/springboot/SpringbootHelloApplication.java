b07fe3e12cd76716776bd12487dad019915912f5
ChenXbFrank <810464826@qq.com>
2020-06-01 22:39:57 +0800

    修改项目的端口，启动不加载数据库

--- src/main/java/com/cxb/springboot/SpringbootHelloApplication.java
+++ src/main/java/com/cxb/springboot/SpringbootHelloApplication.java
@@ -2,8 +2,10 @@ package com.cxb.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// 不加载数据库
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SpringbootHelloApplication {
 
     public static void main(String[] args) {
